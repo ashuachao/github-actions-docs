5cf8b05bae0a19b55371b9090ce0b71f32f8908b
ccchan123456 <545909203@qq.com>
2022-03-20 22:43:49 +0800

    first

---
+++ vue.config.ts
@@ -0,0 +1,6 @@
+// 之后要通过https://autumnwhj.github.io/github-actions-docs/访问，因此资源文件的路径要做对应配置
+// 如果你最终站点访问路径是https://autumnwhj.github.io/docs-other/,则这里的值就是/docs-other/,
+module.exports = {
+    // publicPath 配置指定应用程序中所有资源的基础路径。
+    publicPath: "/github-actions-docs/",
+};
