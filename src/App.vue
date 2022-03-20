3724733c965aa0b4a0c46b5e1b8563b1fa99bdc6
ccchan123456 <545909203@qq.com>
2022-03-20 23:19:19 +0800

    add new content

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  我是新的内容
+  我是新的内容111
 </template>
 
 <script lang="ts">
